fd18bc4523a9283289460301581b0b1de6779037
Ilya <ilialarki@gmail.com>
2021-01-15 07:33:51 +0330

    ➖NyAnnotations

--- nylium-api/src/main/kotlin/io/github/nyliumpowered/nylium/util/NyAnnotations.kt
+++
@@ -1,8 +0,0 @@
-package io.github.nyliumpowered.nylium.util
-
-import kotlin.annotation.AnnotationRetention.*
-import kotlin.annotation.AnnotationTarget.*
-
-@Target(CLASS, FUNCTION, PROPERTY, CONSTRUCTOR, FIELD, ANNOTATION_CLASS)
-@Retention(SOURCE)
-annotation class InternalUseOnly
